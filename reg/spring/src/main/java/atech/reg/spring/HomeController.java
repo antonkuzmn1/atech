bc45c653e9ffc8ea26cf2df1494d1e00743f3319
antonkuzmn1 <antonkuzmn1@gmail.com>
2024-02-16 10:57:33 +0700

    new: Home

---
+++ reg/spring/src/main/java/atech/reg/spring/HomeController.java
@@ -0,0 +1,16 @@
+package atech.reg.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return new String("spring boot is working!");
+    }
+
+}
